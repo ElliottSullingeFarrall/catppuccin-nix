50a2df0e7c8a28180311adf7ac5c1bd60d469a09
ElliottSullingeFarrall <elliott.chalford@gmail.com>
2024-06-29 16:48:27 +0100

    PREBUILD

--- modules/home/catnerd/cursor/default.nix
+++ modules/home/catnerd/cursor/default.nix
@@ -12,7 +12,7 @@ in
   # Causes creation of ~/.icons
   config.home.pointerCursor = lib.mkIf config.catnerd.enable
   {
-    name = "Catppuccin-${lib.catnerd.mkUpper flavour}-${lib.catnerd.mkUpper accent}-Cursors";
+    name = "catppuccin-${flavour}-${accent}-cursors";
     package = pkgs.catppuccin-cursors."${flavour}${lib.catnerd.mkUpper accent}";
     gtk.enable = true;
     size = config.catnerd.cursor.size;

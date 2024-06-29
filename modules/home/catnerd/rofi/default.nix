6d36307a51375676590827b4be0f9581be690859
ElliottSullingeFarrall <elliott.chalford@gmail.com>
2024-06-29 18:03:39 +0100

    PREBUILD

--- modules/home/catnerd/rofi/default.nix
+++ modules/home/catnerd/rofi/default.nix
@@ -12,6 +12,6 @@ in
   config.programs.rofi = lib.mkIf config.catnerd.enable {
     theme = "${pkgs.catnerd.catppuccin-rofi.override { inherit flavour accent; }}";
 
-    font = "${config.catnerd.fonts.main.family} Nerd Font ${builtins.toString config.catnerd.fonts.mono.size}";
+    font = "${config.catnerd.fonts.main.family} Nerd Font ${builtins.toString config.catnerd.fonts.main.size}";
   };
 }
